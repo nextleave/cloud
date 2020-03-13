038c03c3792206ef72adc775f2a84fe489d64c8f
zhudahai <nextleave@gmail.com>
2020-03-13 18:07:33 +0800

    set mq host.

--- src/test/java/com/geblob/cloud/CloudApplicationTests.java
+++ src/test/java/com/geblob/cloud/CloudApplicationTests.java
@@ -5,14 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CloudApplication.class)
 class CloudApplicationTests {
     @Autowired
     private Sender sender;
 
     @Test
-    void contextLoads() {
+    public void send() {
         sender.send();
+    }
+
+    @Test
+    void contextLoads() {
+
     }
 
 }
